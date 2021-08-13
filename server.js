ac165502b0ae1a693cecd7887ccf4fa0b5779d6f
remonroy <remonroy34@gmail.com>
2021-08-13 19:15:05 +0600

    change

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use('/api/users',require('./routers/userRouters'));
 app.use('/api/transactions',require('./routers/transactionRouter'));
 
 
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
-    app.get('*',(req,res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
