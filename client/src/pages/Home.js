5c9ca7088825d158bb44cf4e262c0bc91bd51737
remonroy <remonroy34@gmail.com>
2022-01-09 16:41:03 +0600

    changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-import{connect,useSelector} from 'react-redux'
-import {logout} from '../store/actions/authAction'
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect, useSelector } from "react-redux";
+import { logout } from "../store/actions/authAction";
 
 const Home = (props) => {
-    const state=useSelector(state=>state.auth)
-    return (
-        <div className="row">
-             <div className="col-md-6 ">
-                <img 
-                style={{width:"100%",height:'100%'}}
-                src="https://files.fm/thumb_show.php?i=v2d42qaw5"
-                alt=""
-                />
-            </div>
-            <div className="col-md-6">
-                <a href="/dashboard" className='btn btn-primary mt-5'>Dashboard</a>
-            </div>
-        </div>
-    );
+  const state = useSelector((state) => state.auth);
+  return (
+    <div className="row">
+      <div className="col-md-6 ">
+        <img
+          style={{ width: "70%", height: "70%" }}
+          src="https://res.cloudinary.com/bangla-company/image/upload/v1641724206/twinkls%20cats/images_dypsug.jpg"
+          alt=""
+        />
+      </div>
+      <div style={{ transform: "translateY(25%)" }} className="col-md-6">
+        <a href="/dashboard" className="btn btn-primary mt-5">
+          Dashboard
+        </a>
+      </div>
+    </div>
+  );
 };
 
-export default connect(null,{logout})(Home);
+export default connect(null, { logout })(Home);
--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const passport = require('passport')
-const path =require('path')
-require('dotenv').config()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const path = require("path");
+require("dotenv").config();
 
-const app=express();
+const app = express();
 app.use(morgan());
 app.use(cors());
 app.use(express.json());
-app.use(passport.initialize())
-require('./passport')(passport)
+app.use(passport.initialize());
+require("./passport")(passport);
 
-app.use('/api/users',require('./routers/userRouters'));
-app.use('/api/transactions',require('./routers/transactionRouter'));
+app.use("/api/users", require("./routers/userRouters"));
+app.use("/api/transactions", require("./routers/transactionRouter"));
 
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-app.get('/',(req,res)=>{
-    res.json({
-        message:'Welcome to our application...!'
-    })
-})
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to our application...!",
+  });
+});
 
-const PORT =process.env.PORT || 4000
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dk8do.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    {useNewUrlParser: true, useUnifiedTopology: true,},
-    ()=>{
-        console.log('Database connected successfully...!');
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  mongoose.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dk8do.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+      console.log("Database connected successfully...!");
     }
-    );  
-}) 
+  );
+});
